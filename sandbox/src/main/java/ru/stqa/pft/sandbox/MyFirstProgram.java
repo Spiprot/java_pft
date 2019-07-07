7e3851c7137edc618ee57cccf384ace64a102b34
Spiprot <49907114+Spiprot@users.noreply.github.com>
2019-07-07 23:54:58 +0500

    After_Task2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,23 +5,25 @@ public class MyFirstProgram {
         hello("world");
         hello("user");
         hello("Ayrat");
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-        double a =4;
-        double b=6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+        Point p1 = new Point(3, 0);
+        Point p2 = new Point(2, 0);
+        System.out.println("Расстояние между точками p1 и p2 = " + distance(p1, p2));
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello " + somebody + "!");
     }
 
-    public static double area(double len) {
-        return len * len;
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2.0) + Math.pow((p2.y - p1.y), 2.0));
     }
 
-    public static double area(double a, double b) {
-        return a * b;
-    }
 
 }
+
+
+
