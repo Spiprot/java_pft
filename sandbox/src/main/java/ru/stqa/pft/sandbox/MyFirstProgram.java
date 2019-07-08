4eb79c10edb7fe342163cc0ca2ed24befe96e296
Ayrat Mindubaev <spiprot@bk.ru>
2019-07-08 13:55:52 +0500

    Задание 1-3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,19 +11,10 @@ public class MyFirstProgram {
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
         Point p1 = new Point(3, 0);
         Point p2 = new Point(2, 0);
-        System.out.println("Рассоятоние между точками p1 и p2 = " + distance(p1, p2));
+        System.out.println("Рассоятоние между точками p1 и p2 = " + p1.distance(p2));
     }
 
-    public static void hello(String somebody) {
+    private static void hello(String somebody) {
         System.out.println("Hello " + somebody + "!");
     }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt(Math.pow((p2.x - p1.x), 2.0) + Math.pow((p2.y - p1.y), 2.0));
-    }
-
-
 }
-
-
-
